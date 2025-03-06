184ec40c9f26f3afbd1925082993e141bc3cc937
riakurnian <165978995+riakurnian@users.noreply.github.com>
2025-03-06 09:21:48 +0700

    Update external-package-urls.sh

--- scripts/external-package-urls.sh
+++ scripts/external-package-urls.sh
@@ -64,18 +64,18 @@
     fi
 
     files2+=(
-        "luci-app-3ginfo-lite|https://api.github.com/repos/4IceG/luci-app-3ginfo-lite/releases/latest"
+        #"luci-app-3ginfo-lite|https://api.github.com/repos/4IceG/luci-app-3ginfo-lite/releases/latest"
         "luci-app-adguardhome|https://api.github.com/repos/kongfl888/luci-app-adguardhome/releases/latest"
         "luci-app-alpha-config|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
         "luci-app-base64|https://api.github.com/repos/rtaserver/rta-packages/releases"
         "luci-app-lite-watchdog|https://api.github.com/repos/4IceG/luci-app-lite-watchdog/releases/latest"
-        "luci-app-modemband|https://api.github.com/repos/4IceG/luci-app-modemband/releases/latest"
+        #"luci-app-modemband|https://api.github.com/repos/4IceG/luci-app-modemband/releases/latest"
         "luci-app-netmonitor|https://api.github.com/repos/rtaserver/rta-packages/releases"
         "luci-app-rtaconfig|https://api.github.com/repos/rtaserver/RTA-Theme-OpenWrt/releases/latest"
         #"luci-app-sms-tool-js|https://api.github.com/repos/4IceG/luci-app-sms-tool-js/releases/latest"
         "luci-theme-alpha|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
         "luci-theme-rta|https://api.github.com/repos/rtaserver/RTA-Theme-OpenWrt/releases/latest"
-        "modemband|https://api.github.com/repos/4IceG/luci-app-modemband/releases/latest"
+        #"modemband|https://api.github.com/repos/4IceG/luci-app-modemband/releases/latest"
     )
 
     echo "#########################################"
